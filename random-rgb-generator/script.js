be6fef84b1028edab25360417ad02fb11d9850f0
codesnerd <70039999+codesnerd@users.noreply.github.com>
2022-03-27 21:47:32 +0500

    Add tooltip functions

--- random-rgb-generator/script.js
+++ random-rgb-generator/script.js
@@ -1,11 +1,27 @@
 const body = document.body;
 const h1 = document.querySelector("h1");
+const span = document.querySelector("span");
 const button = document.querySelector("button");
+
+let isColorSet = false;
 
 button.addEventListener("click", () => {
   const color = generateColor();
   setColor(color);
+
+  h1.style.cursor = "pointer";
+
+  enableTooltipVisibility();
 });
+
+/* SET BACKGROUND COLOR */
+
+const setColor = (color) => {
+  body.style.backgroundColor = color;
+  h1.innerText = color;
+
+  isColorSet = true;
+};
 
 const generateColor = () => {
   const r = Math.floor(Math.random() * 256);
@@ -15,7 +31,21 @@ const generateColor = () => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
-const setColor = (color) => {
-  body.style.backgroundColor = color;
-  h1.innerText = color;
+/* HANDLE 'COPY TO CLIPBOARD' TOOLTIP */
+
+const enableTooltipVisibility = () => {
+  span.classList.remove("tooltip-before-color-set");
+  span.classList.add("tooltip-after-color-set");
+  span.innerText = "Copy to Clipboard";
+};
+
+h1.addEventListener("click", () => {
+  if (isColorSet) {
+    copyToClipboard(h1.innerText);
+    span.innerText = "Copied";
+  }
+});
+
+const copyToClipboard = (h1InnerText) => {
+  navigator.clipboard.writeText(h1InnerText);
 };
